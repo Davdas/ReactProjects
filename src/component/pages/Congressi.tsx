72e9e4d4b60c4e48fa51f21d3fc335337c00921d
Davdas <davide_mancini82@yahoo.it>
2023-03-28 15:51:29 +0200

    cambiata scritta

--- src/component/pages/Congressi.tsx
+++ src/component/pages/Congressi.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Congressi = () => {
   return (
     <div className="text-2xl uppercase h-screen flex items-center justify-center">
-      Congressi
+      Congressi ANSPI
     </div>
   );
 };
